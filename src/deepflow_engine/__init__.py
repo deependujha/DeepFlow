08766ffa79507feb655dbc562243bda2cdd240a8
deependujha <deependujha21@gmail.com>
2026-03-24 18:33:42 +0530

    bump v0.1.5

--- src/deepflow_engine/__init__.py
+++ src/deepflow_engine/__init__.py
@@ -5,10 +5,16 @@ from deepflow_engine.core.game import DeepFlowGame
 from deepflow_engine.core.event import Event
 from deepflow_engine.core.pipeline import run_pipeline
 from deepflow_engine.publisher import BasePublisher, TelegramPublisher
+from importlib.metadata import version
+
+
+def get_version() -> str:
+    """Returns the current version of DeepFlow-Engine."""
+    return version("deepflow-engine")
 
 
 def main() -> None:
-    print("Hello from DeepFlow-Engine!")
+    print(f"Hello from DeepFlow-Engine v{get_version()}!")
 
 
 __all__ = [
---
+++ src/deepflow_engine/demos/simple_pygame.py
@@ -0,0 +1,8 @@
+# credits: https://github.com/deependujha
+
+from deepflow_engine.core.game import DeepFlowGame
+
+
+class SimplePyGame(DeepFlowGame):
+    # todo: implement a simple pygame game here to demonstrate the capabilities of DeepFlow-Engine
+    ...
